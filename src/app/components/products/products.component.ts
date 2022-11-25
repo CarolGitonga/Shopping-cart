9928f2e718ec1e45471037ff15bf328bac5f55ab
Carol Gitonga <=>
2022-11-25 10:17:09 +0300

    add filter category

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -9,7 +9,10 @@ import { CartService } from 'src/app/services/cart.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  public filterCategory: any;
   public productList: any;//store all the products coming from the api
+  searchKey: string = "";
+
 //inject the service
   constructor(private api: ApiService, private cartService: CartService) { }
 
@@ -17,15 +20,32 @@ export class ProductsComponent implements OnInit {
     this.api.getProduct()//subscribe the res method
     .subscribe(res=>{//getting the res from productsList
       this.productList = res;
-
-      this.productList.forEach((a: any) => {
+      this.filterCategory = res;
+      this.productList.forEach((a: any) =>{
+        if(a.category ==="women's clothing" || a.category ==="men's clothing"){
+          a.category = "fashion"
+        }
         Object.assign(a,{quantity:1,total:a.price});
       });
-    })
+      console.log(this.productList)
+      });
+
+      this.cartService.search.subscribe((val: any)=>{
+        this.searchKey = val;
+     });   
   }
 
   addtocart(item: any){
     this.cartService.addtoCart(item);
+  }
+
+  filter(category: string){
+    this.filterCategory = this.productList
+    .filter((a: any)=>{
+      if(a.category == category || category==''){
+        return a;
+      }
+    })
 
   }
 
